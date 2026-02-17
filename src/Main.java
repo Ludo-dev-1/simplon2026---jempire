ad6ca3333ed8272bd1b98433dc48134c1c60230b
THIBAUD Ludovic <ludovic.thibaud@maif.fr>
2026-02-17 16:34:44 +0100

    feat : case 3,4,5

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int choice = 0;   
+        System.out.println("Bienvenue dans le jeu de gestion de ressources !");
+        int choice = 0;
         Scanner scanner = new Scanner(System.in);
         Ressources Ressources = new Ressources();
         do {
-        
-        System.out.println("Bienvenue dans le jeu de gestion de ressources !");
-        System.out.println("Ressources actuel : " + Ressources.bois + " bois, " + Ressources.pierre + " pierre, "
-                + Ressources.or + " or, " + Ressources.nourriture + " nourriture, " + Ressources.habitants
-                + " habitants.");
-                
+
+            System.out.println("Ressources actuel : " + Ressources.bois + " bois, " + Ressources.pierre + " pierre, "
+                    + Ressources.or + " or, " + Ressources.nourriture + " nourriture, " + Ressources.habitants
+                    + " habitants.");
+
             System.out.println("Menu principal :");
             System.out.println("1. Explorer la forêt");
             System.out.println("2. Creer une mine");
@@ -22,29 +22,35 @@ public class Main {
             System.out.println("6. Construire un château");
             System.out.println("7. Quitter le jeu");
             System.out.println("Séléctionner une action : ");
-            
+
             choice = scanner.nextInt();
-            boolean mineCreate = false;
+            
 
             switch (choice) {
                 case 1:
                     Ressources.exploreForest();
-
                     break;
                 case 2:
                     Ressources.mine();
-
+                    Ressources.mineCreate = true;
                     break;
                 case 3:
-                    Ressources.workMine();
-                    if (mineCreate == false) {
-                        System.out.println("Vous devez d'abord créer une mine avant de pouvoir y travailler.");
+                    if (Ressources.mineCreate = false || Ressources.nourriture<=5) {
+                        System.out.println("Vous devez d'abord créer une mine avant de pouvoir y travailler ou augmenter vos ressources en nourritures");
+                    } else {
+                        Ressources.workMine();
                     }
                     break;
                 case 4:
+                    if (Ressources.or < 30 ){
+                        System.out.println("Vous devez augmenter vos ressources en or");
+                    }
                     Ressources.recruitSoldier();
                     break;
                 case 5:
+                    if (Ressources.pierre <5 ){
+                        System.out.println("Vous devez augmenter vos ressources en pierre");
+                    }
                     Ressources.commerce();
                     break;
                 case 6:
@@ -52,10 +58,11 @@ public class Main {
                     break;
                 default:
                     System.out.println("Merci d'avoir joué !");
-            }
+            } Ressources.deleteFood();
         } while (choice != 7);
-        
+          
+
         scanner.close();
 
-    } 
+    }
 }
--- src/Ressources.java
+++ src/Ressources.java
@@ -1,46 +1,57 @@
 public class Ressources {
-	int bois;
-	int pierre;
-	int or;
-	int nourriture;
-	int habitants;
-	
-	public Ressources() {
-		this.bois = 0;
-		this.pierre = 0;
+    int bois;
+    int pierre;
+    int or;
+    int nourriture;
+    int habitants;
+    boolean mineCreate;
+
+    public Ressources() {
+        this.bois = 0;
+        this.pierre = 0;
         this.or = 50;
-		this.nourriture = 100;
-		this.habitants = 1;
-		
-	}
-    public void exploreForest(){
+        this.nourriture = 100;
+        this.habitants = 1;
+        this.mineCreate = false;
+
+    }
+
+    public void exploreForest() {
         this.bois += 5;
-        this.nourriture += 3 *habitants;
-       
+        this.nourriture += 3 * habitants;
+
         System.out.println("Vous avez exploré la forêt et récolté 5 bois et 3 nourriture.");
     }
+
     public void mine() {
-         boolean mineCreate = true;
         this.bois -= 10;
         System.out.println("Vous avez crée une mine et débloquer la ressource pierre (-10 bois).");
     }
+
     public void workMine() {
         this.pierre += 5;
         this.nourriture -= 5;
         this.or += 2;
+        this.mineCreate = true;
         System.out.println("Vous avez travaillé dans la mine et récolté 5 pierre et 2 or et consommé 5 nourritures.");
     }
+
     public void recruitSoldier() {
-        this.habitants += 1;
-        this.or -= 30;
-        System.out.println("Vous avez recruté un soldat, ajoute 1 habitant et consomme 30 or.");
+        if (this.or > 30) {
+            this.habitants += 1;
+            this.or -= 30;
+            System.out.println("Vous avez recruté un soldat, ajoute 1 habitant et consomme 30 or.");
+        }
     }
 
     public void commerce() {
-        this.or += 10;
-        this.pierre -= 5;
-        System.out.println("Vous avez fait du commerce et gagné 10 or en échange de 5 pierre.");
+        if(this.pierre > 5){
+            this.or += 10;
+            this.pierre -= 5;
+            System.out.println("Vous avez fait du commerce et gagné 10 or en échange de 5 pierre.");
+        }
     }
+
     public void buildCastle() {
         this.bois -= 100;
         this.pierre -= 100;
@@ -48,6 +59,15 @@ public class Ressources {
         this.habitants -= 40;
         System.out.println("Vous avez construit un château, partie gagné!.");
     }
+
+    public void deleteFood (){
+        if(habitants>nourriture) {
+            int killHabitants = habitants-nourriture;
+            habitants -=killHabitants;
+            System.out.println("Vous n'avez pas assez de nourriture pour subvenir au besoin de vos habitants, vous avez perdu"+ killHabitants+ " habitants");
+        }
+
+        this.nourriture -= 1 * habitants;
+        System.out.println("Vous avez fini un tour de jeu mais les habitants ont mangé, vous avez perdu "+ (habitants) +" de nourritures" );
+    }
 }
-
-
